4af1f121ba997a47d686190eb86e05351b55bce2
piraye-bulut <140714145+piraye-bulut@users.noreply.github.com>
2024-03-31 01:03:05 +0300

    Proje dosyası ekle.

---
+++ Calculator/javaScript.js
@@ -0,0 +1,35 @@
+﻿
+
+let currentInput = '';
+
+function AppendToDisplay(value) {
+    if (value === ',') {
+        value = '.';
+    }
+
+    currentInput += value;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentInput = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('display').value = currentInput;
+}
+
+function calculate() {
+    try {
+        const expression = currentInput.replace(/,/g, '.');
+
+        // Çarpma işlemi için 'x' yerine '*' kullanıldı
+        const result = eval(expression.replace(/x/g, '*'));
+        currentInput = result.toString();
+        updateDisplay();
+    } catch (error) {
+        currentInput = 'Error';
+        updateDisplay();
+    }
+}
